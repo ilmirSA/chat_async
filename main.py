97bcbb13ed2648e711daf2dcc5d36c5582d1acb6
ilmir <ilmir531@gmail.com>
2024-08-17 10:29:19 +0300

    Save all messages to file.

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ async def tcp_echo_client():
     reader, writer = await asyncio.open_connection(
         'minechat.dvmn.org', 5000)
 
-
     while True:
         try:
             data = await reader.read(100)
             print(data.decode())
-        except:
+        except (UnicodeDecodeError):
             pass
-
-
 
 
 async def main():
