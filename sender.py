293e04493c3b3e39af46893d2b8a5bf9d5bf4617
ilmir <ilmir531@gmail.com>
2024-08-17 13:09:09 +0300

    change name module

--- sender.py
+++ sender.py
@@ -1,16 +1,14 @@
-import argparse
 import asyncio
-import datetime
-import os
+import logging
 
-import aiofiles
-from dotenv import load_dotenv
+logger = logging.getLogger(__name__)
 
 
 async def write_to_chat(hash: str, message: str):
     reader, writer = await asyncio.open_connection(
         'minechat.dvmn.org', 5050)
-    await reader.read(100)
+    data = await reader.read(100)
+    logger.debug(data.decode())
     writer.write(f"{hash}\n".encode())
     writer.write(f"{message}\n".encode())
     writer.write("\n".encode())
@@ -19,8 +17,13 @@ async def write_to_chat(hash: str, message: str):
 
 
 async def main():
+    logging.basicConfig(level=logging.DEBUG,
+                        datefmt="%Y-%m-%d %H:%M:%S",
+                        format="[%(asctime)s] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s",
+                        )
     await write_to_chat("98604f8e-5c77-11ef-abed-0242ac110002", "как дела ребята?")
 
 
 if __name__ == '__main__':
+
     asyncio.run(main())
