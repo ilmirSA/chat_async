c5a133890c6d02b81045f2af73b2423aaa3b3289
ilmir <ilmir531@gmail.com>
2024-08-17 10:25:48 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+
+
+async def tcp_echo_client():
+    reader, writer = await asyncio.open_connection(
+        'minechat.dvmn.org', 5000)
+
+
+    while True:
+        try:
+            data = await reader.read(100)
+            print(data.decode())
+        except:
+            pass
+
+
+
+
+async def main():
+    await tcp_echo_client()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
