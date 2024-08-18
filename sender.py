c70d83642c0a29d3f22c89b07b08ed8597a2298f
ilmir <ilmir531@gmail.com>
2024-08-18 12:57:21 +0300

    Setup logging.

--- sender.py
+++ sender.py
@@ -7,23 +7,28 @@ logger = logging.getLogger(__name__)
 async def write_to_chat(hash: str, message: str):
     reader, writer = await asyncio.open_connection(
         'minechat.dvmn.org', 5050)
-    data = await reader.read(100)
+    data = await reader.read(150)
     logger.debug(data.decode())
     writer.write(f"{hash}\n".encode())
+    data = await reader.read(400)
+    logger.debug(data.decode())
     writer.write(f"{message}\n".encode())
+    logger.debug(message)
     writer.write("\n".encode())
     writer.close()
     await writer.wait_closed()
 
 
 async def main():
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(filename='sender.log',level=logging.DEBUG,
                         datefmt="%Y-%m-%d %H:%M:%S",
-                        format="[%(asctime)s] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s",
+                        format="%(levelname)1s:%(module)1s:%(message)s",
+                        encoding="UTF-8"
                         )
-    await write_to_chat("98604f8e-5c77-11ef-abed-0242ac110002", "как дела ребята?")
+    await write_to_chat("98604f8e-5c77-11ef-abed-0242ac110002", "Hello World?")
 
 
 if __name__ == '__main__':
+
 
     asyncio.run(main())
